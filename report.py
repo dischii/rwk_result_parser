39f7b1de2970fe2d9882e10f48584b7ed4d5198c
dischii <adischner@gmail.com>
2024-12-14 12:07:55 +0100

    use static data

--- report.py
+++ report.py
@@ -7,13 +7,26 @@ from competition import Competition
 from team import Team
 
 # testdata - TODO: replace test data with acutal data from results.py
-comp1 = Competition("home", "away", "1:2")
-comp2 = Competition("home2", "away2", "2:1")
-comp3 = Competition("home3", "away3", "3:3")
-comp4 = Competition("home4", "away4", "4:5")
-comps = [comp1, comp2, comp3, comp4]
+comps_for_report = []
 
-team = Team("league", "name", 1, "points", "cut")
+
+comps_for_report.append(Competition("SG Freystadt 1", "SV Wappersdorf 1", "1465:1489"))
+comps_for_report.append(Competition("SV Reichertshofen 2", "SV Wappersdorf 2", "1481:1493"))
+comps_for_report.append(Competition("SV Wappersdorf 3", "SV Sulzkirchen 1", "1401:1416"))
+comps_for_report.append(Competition("SV Wappersdorf 4", "SV Wolfstein 3", "1392:1316"))
+comps_for_report.append(Competition("SV Wappersdorf 5", "SV Hirschberg 2", "1322:1328"))
+comps_for_report.append(Competition("SV Woffenbach 1", "SV Wappersdorf 2", "1045:909"))
+comps_for_report.append(Competition("SV Wappersdorf 1", "SV Woffenbach 2", "964:991"))
+
+teams_for_report = []
+teams_for_report.append(Team("Gauliga 1", "SV Wappersdorf 1", 2, "2:0", 1489.00))
+teams_for_report.append(Team("Gauliga 2", "SV Wappersdorf 2", 1, "2:0", 1493.00))
+teams_for_report.append(Team("A-Klasse 1", "SV Wappersdorf 3", 10, "0:2", 1401.00))
+teams_for_report.append(Team("B-Klasse 2", "SV Wappersdorf 4", 1, "2:0", 1392.00))
+teams_for_report.append(Team("B-Klasse 2", "SV Wappersdorf 5", 7, "0:2", 1322.00))
+teams_for_report.append(Team("Gauoberliga", "SV Wappersdorf 1", 7, "0:2", 964.00))
+teams_for_report.append(Team("Gauoberliga", "SV Wappersdorf 2", 8, "0:2", 909.00))
+
     
 
 def report_competition(ws: xlsxwriter.worksheet, start_row: int, comp_list: list):
@@ -81,12 +94,15 @@ format_tie = workbook.add_format({'bg_color':  '#FFEB9C',
 format_win = workbook.add_format({'bg_color':  '#C6EFCE',
                                  'font_color': '#006100'})
 
-worksheet.write('A1', 'Hello')
-        
-worksheet.write_row(2,0, team.to_list())
+worksheet.write('A1', 'Results from 1st match 2024')
 
-worksheet.write('A5', 'Ergebnisse')
+line = 2 # start at line 2
+for team in teams_for_report:
+    worksheet.write_row(line,0, team.to_list())
+    line += 1
 
-report_competition(worksheet, 5, comps)
+worksheet.write('A10', 'Ergebnisse')
+
+report_competition(worksheet, 10, comps_for_report)
 
 workbook.close()
