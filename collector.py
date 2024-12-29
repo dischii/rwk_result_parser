c0d08aa97374753db6d78dfbb07589b3fab94f4f
Alexander <107007832+dischii@users.noreply.github.com>
2024-12-29 12:58:18 +0100

    feat: automate md result files (#5)
    * feat: add events and md report
    
    * feat: allow download of different rounds
    
    * fix: empty results
    
    * feat: allow different competition days
    
    * fix: data download
    
    * fix: file_names for markdown
    
    * docs: update readme

--- collector.py
+++ collector.py
@@ -1,4 +1,5 @@
 """ This script is used to download the HTML of the RWK shooting website. """
+import os
 from bs4 import BeautifulSoup
 
 from selenium import webdriver
@@ -7,7 +8,7 @@ from selenium.webdriver.support.ui import Select
 
 XPATH_LOGIN_AUSWAHL = '/html/body/div/div/form[1]/div/input'
 XPATH_ANZEIGEN_HTML = '//*[@id="content"]/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[13]/td/form[2]/input[2]'
-
+XPATH_RUNDEN_ID = '//*[@id="content"]/table/tbody/tr/td/table/tbody/tr/td[2]/table/tbody/tr[8]/td[2]/select'
 
 def download_html():
     """ Downloads the HTML of the RWK shooting website. """
@@ -25,13 +26,34 @@ def download_html():
     select = Select(select_element)
     select.select_by_value("SV Wappersdorf:1")
 
-    driver.find_element(By.XPATH, XPATH_ANZEIGEN_HTML).click()
+    file_names = []
 
-    driver.switch_to.frame("drucken")
-    iframe_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    with open("output.html", "w", encoding="utf-8") as file:
-        file.write(iframe_soup.prettify())
+    # get all rounds
+    select_element = driver.find_element(By.XPATH, XPATH_RUNDEN_ID)
+    select = Select(select_element)
+    cnt_rounds = len(select.options)
 
-    driver.switch_to.default_content()
+    for i in range(0, cnt_rounds-1):
+        select_element = driver.find_element(By.XPATH, XPATH_RUNDEN_ID)
+        select = Select(select_element)
+        select_options = select.options
+        round_value = select_options[i].get_attribute("value")
+        select.select_by_value(round_value)
+
+        driver.find_element(By.XPATH, XPATH_ANZEIGEN_HTML).click()
+
+        driver.switch_to.frame("drucken")
+        iframe_soup = BeautifulSoup(driver.page_source, 'html.parser')
+        os.makedirs('temp', exist_ok=True)
+        file_names.append(f"temp/{round_value.replace(':','_').lower()}.html")
+        with open(file_names[len(file_names)-1], "w", encoding="utf-8") as file:
+            file.write(iframe_soup.prettify())
+
+        driver.switch_to.default_content()
 
     driver.close()
+
+    return file_names
+
+if __name__ == '__main__':
+    download_html()
--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 """ Main file to run the program. """
-
+import os
 import collector
+
 from xlsx_report import XLSXReport
 from result_parser import ResultParser
-
+from md_report import MDReport
 
 # Download the HTML content
-collector.download_html()
+html_result = collector.download_html()
 
-# Parse the HTML content
-parser = ResultParser()
-parser.print_groups_for_team()
-competitions = parser.get_competitions_from_team()
-teams = parser.get_team_info()
+for html in html_result:
+    # Parse the HTML content
+    parser = ResultParser(html)
+    events = parser.get_event_infos()
+    competitions = parser.get_competitions_from_team()
+    teams = parser.get_team_info()
 
-# Print parsed results
-print(teams)
-print(competitions)
+    # Create Markdown
+    mdfilename = os.path.basename(html).split(".")[0]
+    mdfilename = f"svw-results/docs/results_{mdfilename}"
+    md = MDReport(mdfilename, f"{events[0].competition_string}")
+    md.report_competition(competitions)
+    md.report_team(teams)
+    md.add_file_links("files/report.xlsx", "Excel Report")
+    md.create_file()
 
-# Create a report
-report = XLSXReport()
-report.report_competition(competitions)
-report.report_team(teams)
-report.close()
+    # Create a report
+    report = XLSXReport(filename=html.replace("html", "xlsx"), header=f"Results from {events[0].competition_string}")
+    report.report_competition(competitions)
+    report.report_team(teams)
+    report.close()
